d28e97d30accb926d027afff684f94735bb06076
lueckekm <lueckekm@miamioh.edu>
2019-09-30 13:12:35 -0400

    tester for constructor done

--- StockTester.java
+++ StockTester.java
@@ -6,8 +6,35 @@ class StockTester {
 
     @Test
     void testStockClass() {
+        //test default constructor
         Stock stock = new Stock();
+        
+        //test name
         stock.setName("name");
         assertEquals(stock.getName(), "name");
+        
+        //test description
+        stock.setDescription("description");
+        assertEquals(stock.getDescription(), "description");
+        
+      //test index
+        stock.setIndex("index");
+        assertEquals(stock.getIndex(), "index");
+        
+      //test value
+        stock.setValue(123.45);
+        assertEquals(stock.getValue(), 123.45);
+        
+      //test description
+        stock.setDescription("description");
+        assertEquals(stock.getDescription(), "description");
+        
+      //test trend
+        stock.setTrend(67.89);
+        assertEquals(stock.getTrend(), 67.89);
+        
+        //test RQE
+        stock.setRQE(98.7);
+        assertEquals(stock.getRQE(), 98.7);
     }
 }
