10d8d8c4bc8ede16371913be376beef70d7648f6
lueckekm <lueckekm@miamioh.edu>
2019-09-30 13:05:54 -0400

    help

--- StockTester.java
+++ StockTester.java
@@ -7,6 +7,7 @@ class StockTester {
     @Test
     void testStockClass() {
         Stock stock = new Stock();
+        stock.setName("name");
+        assertEquals(stock.getName(), "name");
     }
-
 }
