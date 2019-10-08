1522d5ad2151e631fb1cca13790889d53ec3dfd2
lueckekm <lueckekm@miamioh.edu>
2019-10-08 18:13:44 -0400

    Start of Admin class

---
+++ Admin.java
@@ -0,0 +1,32 @@
+
+public class Admin {
+    String userName;
+    int userId;
+    String password;
+    
+    public Admin() {
+        userName = "";
+        userId = -1;
+        password = "";
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
