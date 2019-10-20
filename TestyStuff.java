993afb68d62c68c54009784deaf1e59e1ca7c9d0
David Helbling <helblide@miamioh.edu>
2019-10-20 14:52:29 -0400

    Beginning GUI

--- TestyStuff.java
+++ TestyStuff.java
@@ -12,6 +12,9 @@ import javax.swing.JSplitPane;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.BoxLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 
 public class TestyStuff extends JFrame {
 
@@ -46,40 +49,51 @@ public class TestyStuff extends JFrame {
         contentPane.setLayout(new BorderLayout(0, 0));
         
         JPanel Bowse_panel = new JPanel();
-        contentPane.add(Bowse_panel, BorderLayout.CENTER);
+        contentPane.add(Bowse_panel);
         Bowse_panel.setLayout(new BoxLayout(Bowse_panel, BoxLayout.X_AXIS));
         
         JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setAlignmentY(Component.TOP_ALIGNMENT);
+        scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
         Bowse_panel.add(scrollPane);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         
         JLabel lblStockBowse_1 = new JLabel("Stock Bowse");
         scrollPane.setColumnHeaderView(lblStockBowse_1);
         
-        JPanel StockPanel = new JPanel();
-        scrollPane.setViewportView(StockPanel);
+        JPanel panel = new JPanel();
+        contentPane.add(panel, BorderLayout.NORTH);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
         
         JSplitPane splitPane = new JSplitPane();
-        StockPanel.add(splitPane);
-        
-        JPanel panel = new JPanel();
-        splitPane.setLeftComponent(panel);
-        
-        JSplitPane splitPane_1 = new JSplitPane();
-        splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
-        panel.add(splitPane_1);
-        
-        JLabel lblStockName = new JLabel("Stock Name");
-        splitPane_1.setLeftComponent(lblStockName);
-        
-        JLabel lblStockAbb = new JLabel("Stock Abb");
-        splitPane_1.setRightComponent(lblStockAbb);
+        panel.add(splitPane);
         
         JPanel panel_1 = new JPanel();
         splitPane.setRightComponent(panel_1);
+        panel_1.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
         
-        JLabel lblStockInfo = new JLabel("Stock Info");
-        panel_1.add(lblStockInfo);
+        JButton btnLogin = new JButton("Login");
+        panel_1.add(btnLogin);
+        
+        JButton btnSignup = new JButton("Signup");
+        panel_1.add(btnSignup);
+        
+        JPanel panel_2 = new JPanel();
+        splitPane.setLeftComponent(panel_2);
+        panel_2.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        
+        JLabel lblStockItTo = new JLabel("Stock It To Me");
+        panel_2.add(lblStockItTo);
+        
+        JPanel panel_3 = new JPanel();
+        FlowLayout flowLayout = (FlowLayout) panel_3.getLayout();
+        flowLayout.setVgap(15);
+        flowLayout.setHgap(15);
+        flowLayout.setAlignment(FlowLayout.LEFT);
+        contentPane.add(panel_3, BorderLayout.EAST);
+        
+        JScrollPane scrollPane_1 = new JScrollPane();
+        panel_3.add(scrollPane_1);
     }
 
 }
