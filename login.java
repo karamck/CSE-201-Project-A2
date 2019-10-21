92ce79c88c1d09f6dab412bec9d29be8eb773c9f
lueckekm <lueckekm@miamioh.edu>
2019-10-21 12:50:50 -0400

    Revert "Revert "Revert "Revert "Update login.java""""
    This reverts commit 682688adc03d7d816212698608959cc308682005.

--- login.java
+++ login.java
@@ -16,6 +16,8 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class login extends JFrame {
 
@@ -77,6 +79,17 @@ public class login extends JFrame {
 		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		
+		btnLogin.addActionListener(new ActionListener()
+	    {
+	      public void actionPerformed(ActionEvent e)
+	      {
+	        // print login attempt
+	        System.out.print("Username: " + textField.getText() + "\nPassword: " + passwordField.getText());
+	      }
+	    });
+		
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
