f99aae5aa3bbd152b798cfd146c7e7e305b0ad4a
lueckekm <lueckekm@miamioh.edu>
2019-10-21 13:39:48 -0400

    changes

--- StockTester.java
+++ StockTester.java
@@ -38,4 +38,46 @@ class StockTester {
         assertEquals(stock.getRQE(), 98.7);
         //comment
     }
+    
+    @Test
+    void testUserClass() {
+        //test default constructor
+        User user = new User();
+        
+        //test name
+        user.setUserName("name");
+        assertEquals(user.getUserName(), "name");
+        
+        //test Password
+        user.setPassword("password");
+        assertEquals(user.getPassword(), "password");
+        
+      //test UserId
+        user.setUserId(1202);
+        assertEquals(user.getUserId(), 1202);
+        
+    }
+     
+    @Test
+    void testAdminClass() {
+        Admin admin = new Admin();
+
+        // test name
+
+        admin.setUserName("name");
+        assertEquals(admin.getUserName(), "name");
+
+        // test Password
+        admin.setPassword("password");
+        assertEquals(admin.getPassword(), "password");
+
+        // test adminId
+        admin.setUserId(1202);
+        assertEquals(admin.getUserId(), 1202);
+    }
+    
+    @Test
+    void dbTest() {
+        assertEquals(UserNameDB.checkUser("admin", "password"), true);
+    }
 }
--- UserTester.java
+++ UserTester.java
@@ -21,4 +21,25 @@ class UserTester {
         assertEquals(user.getUserId(), 1202);
         
     }
+    
+    void testAdminClass() {
+        Admin admin = new Admin();
+
+        // test name
+
+        admin.setUserName("name");
+        assertEquals(admin.getUserName(), "name");
+
+        // test Password
+        admin.setPassword("password");
+        assertEquals(admin.getPassword(), "password");
+
+        // test adminId
+        admin.setUserId(1202);
+        assertEquals(admin.getUserId(), 1202);
+    }
+    
+    void dbTest() {
+        assertEquals(UserNameDB.checkUser("admin", "password"), true);
+    }
 }
