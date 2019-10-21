7c3d103a8a84631cba6549fdc9e0500a7d885b9e
lueckekm <lueckekm@miamioh.edu>
2019-10-21 12:45:07 -0400

    might fail

--- login.java
+++ login.java
@@ -16,12 +16,15 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
 	private JPasswordField passwordField;
 	private JTextField textField;
+	private String un;
 
 	/**
 	 * Launch the application.
@@ -38,6 +41,7 @@ public class login extends JFrame {
 			}
 		});
 	}
+	
 
 	/**
 	 * Create the frame.
@@ -59,9 +63,19 @@ public class login extends JFrame {
 		passwordField = new JPasswordField();
 		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
+	
 		textField = new JTextField();
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		textField.setColumns(10);
+		textField.addActionListener(new ActionListener(){
+		   
+            public void actionPerformed(ActionEvent cl){
+                System.out.println(textField.getText());
+                un = textField.getText();
+                System.out.println(un);
+            }
+        });
+		
 		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 18));
