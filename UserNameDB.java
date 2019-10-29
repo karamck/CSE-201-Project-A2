bc77954a5b4c86eb0411fe2c16915ecc1ad7e9d3
RapsarKM <33526293+RapsarKM@users.noreply.github.com>
2019-10-29 17:38:53 -0400

    Update UserNameDB.java

--- UserNameDB.java
+++ UserNameDB.java
@@ -52,7 +52,14 @@ public class UserNameDB {
         return false;
     }
     
-    public static boolean addUser(String userName, String password) throws IOException {
+    public static int addUser(String userName, String password) throws IOException {
+    	/* return of 0 is succesful
+    	 * return of 1 is invailid username or password
+    	 * return of 2 is username already taken
+    	 * default return is 3
+    	 */
+    	if (!validate(userName)||!validate(password)) return 1;
+    	if (checkUsername(userName)) return 2;
         File unDB = new File("Usernames_Passwords.txt");
         try {
             FileWriter fw = new FileWriter(unDB, true);
@@ -60,11 +67,37 @@ public class UserNameDB {
             br.write("\n" + userName + " " + password);
             br.close();
             fw.close();
-            return true;
+            return 0;
         } catch (FileNotFoundException exception) {
             System.out.println("File not found");
         }
-        System.out.println("User Not Found");
+        System.out.println("Cant Create User");
+        return 3;
+    }
+    
+    private static boolean validate(String string) {
+    	if (string.contains(" ")) return false;
+    	return true;
+    }
+    
+    private static boolean checkUsername(String userName) {
+    	File unDB = new File("Usernames_Passwords.txt");
+        try {
+            Scanner dbReader = new Scanner(unDB);
+            
+            while(dbReader.hasNextLine()) {
+                String line = dbReader.nextLine();
+                String[] splitLine = line.split(" ");
+                String lineUserName = splitLine[0];
+                String linePassword = splitLine[1];
+                if (userName.equals(lineUserName)) {
+                    return true;
+                }
+            }
+            dbReader.close();
+        } catch (FileNotFoundException exception) {
+            System.out.println("File not found");
+        }
         return false;
     }
 }
