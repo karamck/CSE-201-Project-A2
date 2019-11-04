8073252088422d1005a8d983341a46078e31d35e
karamck <karamck@miamioh.edu>
2019-11-04 18:25:36 -0500

    Update APIattempt.java

--- APIattempt.java
+++ APIattempt.java
@@ -1,13 +1,13 @@
 
 import java.io.IOException;
 import java.math.BigDecimal;
-
 import yahoofinance.YahooFinance;
+
 public class APIattempt {
 
 	public static void main(String[] args) throws IOException {
 		yahoofinance.Stock stock = YahooFinance.get("ATNM");
-
+		
 		BigDecimal price = stock.getQuote().getPrice();
 		System.out.println("below is the price");
 		System.out.println(price);
