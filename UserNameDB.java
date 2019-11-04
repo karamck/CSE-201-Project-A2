59fbb031bca1f881c5c631ecab5505e6efadf2ae
karamck <karamck@miamioh.edu>
2019-11-04 18:24:58 -0500

    Update UserNameDB.java

--- UserNameDB.java
+++ UserNameDB.java
@@ -4,6 +4,10 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserNameDB {
 
@@ -24,6 +28,7 @@ public class UserNameDB {
      * @return true if successful, false otherwise
      */
     public static boolean checkUser(String userName, String password) {
+    	password = encrypt(password);
         File unDB = new File("Usernames_Passwords.txt");
         try {
             Scanner dbReader = new Scanner(unDB);
@@ -52,7 +57,15 @@ public class UserNameDB {
         return false;
     }
     
-    public static boolean addUser(String userName, String password) throws IOException {
+    public static int addUser(String userName, String password) throws IOException {
+    	/* return of 0 is successful
+    	 * return of 1 is invalid username or password
+    	 * return of 2 is username already taken
+    	 * default return is 3
+    	 */
+    	password = encrypt(password);
+    	if (!validate(userName)||!validate(password)) return 1;
+    	if (checkUsername(userName)) return 2;
         File unDB = new File("Usernames_Passwords.txt");
         try {
             FileWriter fw = new FileWriter(unDB, true);
@@ -60,11 +73,58 @@ public class UserNameDB {
             br.write("\n" + userName + " " + password);
             br.close();
             fw.close();
-            return true;
+            return 0;
         } catch (FileNotFoundException exception) {
             System.out.println("File not found");
         }
-        System.out.println("User Not Found");
+        System.out.println("Cant Create User");
+        return 3;
+    }
+    
+    private static boolean validate(String string) {
+    	if (string.contains(" ")) return false;
+    	return true;
+    }
+    
+    private static boolean checkUsername(String userName) {
+    	File unDB = new File("Usernames_Passwords.txt");
+        try {
+            Scanner dbReader = new Scanner(unDB);
+            
+            while(dbReader.hasNextLine()) {
+                String line = dbReader.nextLine();
+                String[] splitLine = line.split(" ");
+                String lineUserName = splitLine[0];
+                if (userName.equals(lineUserName)) {
+                    return true;
+                }
+            }
+            dbReader.close();
+        } catch (FileNotFoundException exception) {
+            System.out.println("File not found");
+        }
         return false;
     }
+    
+   private static String encrypt(String str) {
+	   final String SALT = "Look at me im encryption";
+	   String input = SALT + str;
+	   StringBuilder hash = new StringBuilder();
+
+		try {
+			MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			byte[] hashedBytes = sha.digest(input.getBytes());
+			char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+					'a', 'b', 'c', 'd', 'e', 'f' };
+			for (int idx = 0; idx < hashedBytes.length; ++idx) {
+				byte b = hashedBytes[idx];
+				hash.append(digits[(b & 0xf0) >> 4]);
+				hash.append(digits[b & 0x0f]);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			// handle error here.
+		}
+		return hash.toString();
+   }
+   
 }
