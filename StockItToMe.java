13c6dae0f53c664d444ae8bb0fc23e190aa1ac9e
karamck <karamck@miamioh.edu>
2019-11-25 19:00:46 -0500

    stocks added to text file

--- StockItToMe.java
+++ StockItToMe.java
@@ -148,7 +148,12 @@ public class StockItToMe extends JFrame{
 							}
                     		String[] theirs = user.getStock();
                     		System.out.println(Arrays.toString(theirs));
-                    		fillListModel(currentUser, portfolio);
+                    		try {
+								fillListModel(currentUser, portfolio);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
                     	}
                     	counter++;
                     }
@@ -332,7 +337,12 @@ public class StockItToMe extends JFrame{
                     		currentUser.removeStock(s);
                     		String[] theirs = currentUser.getStock();
                     		System.out.println(Arrays.toString(theirs));
-                    		fillListModel(currentUser, portfolio);
+                    		try {
+								fillListModel(currentUser, portfolio);
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
                     	}
                     	counter++;
                     }
